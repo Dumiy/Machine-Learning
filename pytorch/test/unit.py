0555db2088b9c495d0809c4a6d9a264f96052c93
Dumiy <45998406+Dumiy@users.noreply.github.com>
2021-03-29 20:11:23 +0300

    Added unittest and NeuralNetwork class

---
+++ pytorch/test/unit.py
@@ -0,0 +1,39 @@
+import unittest
+import torch
+from ..models.neuralnet import NeuralNet
+
+
+class Class(unittest.TestCase):
+    def test_simple_neural_network(self):
+        model = NeuralNet(100, [50, 60, 80], [torch.nn.ReLU(), torch.nn.ReLU(), torch.nn.ReLU()])
+        model.eval()
+        self.assertIsInstance(model, NeuralNet, "Should create a NeuralNet class")
+
+    def test_complex_neural_network(self):
+        model = NeuralNet(5, [15, 20, 2],
+                          [[torch.nn.ReLU(), torch.nn.BatchNorm1d(15)], [torch.nn.ReLU(), torch.nn.BatchNorm1d(20)],
+                           torch.nn.Sigmoid()])
+        model.eval()
+        self.assertIsInstance(model, NeuralNet, "Should create a NeuralNet class")
+
+    def test_run_simple_neural_network(self):
+        model = NeuralNet(100, [50, 60, 80], [torch.nn.ReLU(), torch.nn.ReLU(), torch.nn.ReLU()])
+        model.eval()
+        var = torch.randn(1, 100, requires_grad=False)
+        self.assertEqual(model(var).shape, (1, 80), "Should create a NeuralNet class")
+
+    def test_run_complex_neural_network(self):
+        model = NeuralNet(5, [15, 20, 2],
+                          [[torch.nn.ReLU(), torch.nn.BatchNorm1d(15)], [torch.nn.ReLU(), torch.nn.BatchNorm1d(20)],
+                           torch.nn.Sigmoid()])
+        model.eval()
+        var = torch.randn(1, 5, requires_grad=False)
+        self.assertEqual(model(var).shape, (1, 2), "Should create a NeuralNet class")
+
+
+def call_main():
+    unittest.main()
+
+
+if __name__ == '__main__':
+    unittest.main()
